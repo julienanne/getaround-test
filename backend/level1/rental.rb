8d0c868a79de6fed0d9a4bbea3541735f3b2bf47
Julien ANNE <contact@julien-anne.fr>
2021-07-20 12:58:41 +0200

    Feat/1_Level1

---
+++ backend/level1/rental.rb
@@ -0,0 +1,27 @@
+require 'date'
+
+class Rental
+  attr_accessor :id
+  attr_accessor :car
+  attr_accessor :start_date
+  attr_accessor :end_date
+  attr_accessor :distance
+
+  def initialize(id, car, start_date, end_date, distance)
+    self.id = id
+    self.car = car
+    self.start_date = Date.strptime(start_date)
+    self.end_date = Date.strptime(end_date)
+    self.distance = distance
+  end
+
+  def get_price
+    # The start and end day is included so + 1
+    rent_day = (self.end_date - self.start_date) + 1
+    (rent_day * self.car["price_per_day"] + self.distance * self.car["price_per_km"]).to_i
+  end
+
+  def to_output_json
+    return { "id": self.id, "price": self.get_price }
+  end
+end
--- backend/spec/level1/main_spec.rb
+++ backend/spec/level1/main_spec.rb
@@ -1,12 +1,22 @@
 require_relative '../../level1/main'
 
 RSpec.describe "Main" do
-  describe "can execute" do
-    context "with Hello" do
-      it "return Hello on stdout" do
-        sentence_regex = /^Hello\n$/
-        expect { execute(["Hello"]) }.to output(sentence_regex).to_stdout
-      end
+  describe "execute" do
+    it "should create a File named data/output.json" do
+      execute
+
+      expect(File).to exist("./level1/data/output.json")
+    end
+
+    it "should populate the File named data/output.json with the rentals array containing the price from the input file data/input.json and corresponding to the expected output file" do
+      execute
+
+      expected_output = File.read("./level1/data/expected_output.json")
+      actual_output = File.read("./level1/data/output.json")
+
+      expect(actual_output).to eq(expected_output)
     end
   end
+
+
 end
