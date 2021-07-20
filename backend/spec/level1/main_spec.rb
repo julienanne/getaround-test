1e34643ae4e61cbda0f398d2dac2e2047d3c2a20
Julien ANNE <contact@julien-anne.fr>
2021-07-20 12:16:25 +0200

    Add Rental class with instanciation, relation with a hash car and get_price function

--- backend/spec/level1/main_spec.rb
+++ backend/spec/level1/main_spec.rb
@@ -1,31 +1,38 @@
 require_relative '../../level1/main'
 
 RSpec.describe "Main" do
-  describe "can execute" do
-    context "for level1" do
-      it "create a File named data/output.json" do
-        execute
+  describe "execute" do
+    it "should create a File named data/output.json" do
+      execute
 
-        expect(File).to exist("./level1/data/output.json")
-      end
-
-      it "populate the File named data/output.json with the rentals array" do
-        execute("./level1/data/input2.json")
-
-        expected_output = "{\n  \"rentals\": [\n    {\n      \"id\": 1\n    },\n    {\n      \"id\": 2\n    }\n  ]\n}\n"
-        actual_output = File.read("./level1/data/output.json")
-
-        expect(actual_output).to eq(expected_output)
-      end
-
-      it "populate the File named data/output.json with the rentals array from the input file data/input.json" do
-        execute
-
-        expected_output = "{\n  \"rentals\": [\n    {\n      \"id\": 1\n    },\n    {\n      \"id\": 2\n    },\n    {\n      \"id\": 3\n    }\n  ]\n}\n"
-        actual_output = File.read("./level1/data/output.json")
-
-        expect(actual_output).to eq(expected_output)
-      end
+      expect(File).to exist("./level1/data/output.json")
     end
+
+    it "should populate the File named data/output.json with the rentals array" do
+      execute("./level1/data/input2.json")
+
+      expected_output = "{\n  \"rentals\": [\n    {\n      \"id\": 1\n    },\n    {\n      \"id\": 2\n    }\n  ]\n}\n"
+      actual_output = File.read("./level1/data/output.json")
+
+      expect(actual_output).to eq(expected_output)
+    end
+
+    it "should populate the File named data/output.json with the rentals array from the input file data/input.json" do
+      execute
+
+      expected_output = "{\n  \"rentals\": [\n    {\n      \"id\": 1\n    },\n    {\n      \"id\": 2\n    },\n    {\n      \"id\": 3\n    }\n  ]\n}\n"
+      actual_output = File.read("./level1/data/output.json")
+
+      expect(actual_output).to eq(expected_output)
+    end
+
+    # it "should populate the File named data/output.json with the rentals array containing the price from the input file data/input.json and corresponding to the expected output file" do
+    #   execute
+
+    #   expected_output = File.read("./level1/data/expected_output.json")
+    #   actual_output = File.read("./level1/data/output.json")
+
+    #   expect(actual_output).to eq(expected_output)
+    # end
   end
 end
---
+++ backend/spec/level1/rental_spec.rb
@@ -0,0 +1,31 @@
+require_relative "../../level1/rental"
+
+RSpec.describe Rental, "object" do
+  context "is instanciate" do
+    it "with the informations" do
+      rental_car = { id: 1, price_per_day: 2000, price_per_km: 10 }
+      rental = Rental.new(1, rental_car, "2017-12-8", "2017-12-10", 100)
+      expect(rental.id).to eq(1)
+      expect(rental.car).to eq({id: 1, price_per_day: 2000, price_per_km: 10})
+      expect(rental.start_date).to eq(Date.strptime("2017-12-8"))
+      expect(rental.end_date).to eq(Date.strptime("2017-12-10"))
+      expect(rental.distance).to eq(100)
+    end
+  end
+
+  context "can calculate price" do
+    it "with distance to zero" do
+      rental_car = { id: 1, price_per_day: 2000, price_per_km: 10 }
+      rental = Rental.new(1, rental_car, "2017-12-8", "2017-12-10", 0)
+
+      expect(rental.get_price).to eq(6000)
+    end
+
+    it "with distance not zero" do
+      rental_car = { id: 1, price_per_day: 2000, price_per_km: 10 }
+      rental = Rental.new(1, rental_car, "2017-12-8", "2017-12-10", 100)
+
+      expect(rental.get_price).to eq(7000)
+    end
+  end
+end
