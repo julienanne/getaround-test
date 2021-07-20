5c522ae1c79a157958a50053f66a6bdfec02c703
Julien ANNE <contact@julien-anne.fr>
2021-07-20 14:44:28 +0200

    Refactoring on Rental class

--- backend/level2/rental.rb
+++ backend/level2/rental.rb
@@ -18,26 +18,32 @@ class Rental
   def get_price
     # The start and end day is included so + 1
     rent_day = (self.end_date - self.start_date) + 1
-
-    price_per_day_base = self.car["price_per_day"]
-    price_per_day_after_10_days = price_per_day_base - (price_per_day_base * 50 / 100)
-    price_per_day_after_4_days = price_per_day_base - (price_per_day_base * 30 / 100)
-    price_per_day_after_1_day = price_per_day_base - (price_per_day_base * 10 / 100)
-
     days_after_10 = rent_day > 10 ? rent_day - 10 : 0
     days_after_4 = rent_day > 4 ? rent_day - 4 - days_after_10 : 0
     days_after_1 = rent_day > 1 ? rent_day - 1 - days_after_4 - days_after_10 : 0
 
-    price_after_10_days = days_after_10 * price_per_day_after_10_days
-    price_after_4_days = days_after_4 * price_per_day_after_4_days
-    price_after_1_day = days_after_1 * price_per_day_after_1_day
+    price_per_day_base = self.car["price_per_day"]
 
-    price_kms = self.distance * self.car["price_per_km"]
-
-    return (price_after_10_days + price_after_4_days + price_after_1_day + price_per_day_base + price_kms).to_i
+    return (
+      (price_per_day_base * rent_day) -
+      discount_after_days(days_after_10, 50) -
+      discount_after_days(days_after_4, 30) -
+      discount_after_days(days_after_1, 10) +
+      price_kms
+      ).to_i
   end
 
   def to_output_json
     return { "id": self.id, "price": self.get_price }
+  end
+
+  private
+  def discount_after_days(days, percent)
+    discount_per_day = (self.car["price_per_day"] * percent / 100)
+    return days * discount_per_day
+  end
+
+  def price_kms
+    self.distance * self.car["price_per_km"]
   end
 end
