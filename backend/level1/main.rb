aa28250f7c02b5b7faadc974f190b812943a9aea
Julien ANNE <contact@julien-anne.fr>
2021-07-20 11:43:59 +0200

    Generate output json file in the right format with rentals id

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,9 +1,33 @@
-require 'fileutils'
+require 'json'
 
-def execute
-  actual_dir = File.expand_path(File.dirname(__FILE__))
+ACTUAL_DIR = File.expand_path(File.dirname(__FILE__))
 
-  FileUtils.touch(File.join(actual_dir, 'data/output.json'))
+def execute(input_path = nil)
+  input_json = get_json_from_file(input_path)
+
+  rentals = get_id_for_rentals(input_json)
+
+  write_output_json_file(rentals)
+end
+
+def get_json_from_file(input_path)
+  input_file_path = input_path || File.join(ACTUAL_DIR, 'data/input.json')
+  return JSON.parse(File.read(input_file_path))
+end
+
+def get_id_for_rentals(input_json)
+  return input_json["rentals"].map do |rental|
+    rental.select { |attribute, value| attribute == "id" }
+  end
+end
+
+def write_output_json_file(rentals)
+  output_json = JSON.generate({ "rentals" => rentals },
+    { indent: "  ", space: " ", array_nl: "\n", object_nl: "\n" })
+
+  File.open(File.join(ACTUAL_DIR, 'data/output.json'), "w") do |file|
+    file.puts(output_json)
+  end
 end
 
 if $0 == __FILE__
--- backend/spec/level1/main_spec.rb
+++ backend/spec/level1/main_spec.rb
@@ -8,6 +8,24 @@ RSpec.describe "Main" do
 
         expect(File).to exist("./level1/data/output.json")
       end
+
+      it "populate the File named data/output.json with the rentals array" do
+        execute("./level1/data/input2.json")
+
+        expected_output = "{\n  \"rentals\": [\n    {\n      \"id\": 1\n    },\n    {\n      \"id\": 2\n    }\n  ]\n}\n"
+        actual_output = File.read("./level1/data/output.json")
+
+        expect(actual_output).to eq(expected_output)
+      end
+
+      it "populate the File named data/output.json with the rentals array from the input file data/input.json" do
+        execute
+
+        expected_output = "{\n  \"rentals\": [\n    {\n      \"id\": 1\n    },\n    {\n      \"id\": 2\n    },\n    {\n      \"id\": 3\n    }\n  ]\n}\n"
+        actual_output = File.read("./level1/data/output.json")
+
+        expect(actual_output).to eq(expected_output)
+      end
     end
   end
 end
