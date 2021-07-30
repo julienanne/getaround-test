71f35655c985a7b2304fd4efc0b27a2bb4db42a6
Julien ANNE <contact@julien-anne.fr>
2021-07-30 20:50:13 +0200

    Base Backend V2

---
+++ backendv2/monitor.sh
@@ -0,0 +1,19 @@
+files=$(find ./ -type f -name "*.rb")
+
+echo "Monitoring..."
+echo "$files"
+
+clear
+
+while true; do
+  echo ""
+  echo ""
+  echo $(date)
+  # bundle exec rspec # for all test but level incompatible
+  # bundle exec rspec --pattern spec/level1/*_spec.rb # for level1 tests
+  # bundle exec rspec --pattern spec/level2/*_spec.rb # for level2 tests
+  # bundle exec rspec --pattern spec/level3/*_spec.rb # for level3 tests
+  # bundle exec rspec --pattern spec/level4/*_spec.rb # for level4 tests
+  # bundle exec rspec --pattern spec/level5/*_spec.rb # for level5 tests
+  inotifywait -qq -e close_write $files
+done
