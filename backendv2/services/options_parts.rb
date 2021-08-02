7a1b140a3045c7bcb497f7e17bddc85f8e0717b6
Julien ANNE <contact@julien-anne.fr>
2021-08-02 12:19:34 +0200

    Level 5 complete comments delete

--- backendv2/services/options_parts.rb
+++ backendv2/services/options_parts.rb
@@ -1,30 +1,10 @@
 require_relative 'service'
 
-# Not open/closed principle so require at runtime
-# require_relative '../models/gps'
-# require_relative '../models/baby_seat'
-# require_relative '../models/additional_insurance'
-
 class OptionsParts < Service
   def initialize(rental_days:, options:)
     @rental_days = rental_days
     @options = options
   end
-
-  # def call
-  #   # Not open/closed principle to do so polymorphism on options by type with Factory or meta programming
-  #   options_parts = { owner: 0, drivy: 0 }
-  #   @options.each do |option|
-  #     if option == "gps"
-  #       options_parts[:owner] += 500 * @rental_days
-  #     elsif option == "baby_seat"
-  #       options_parts[:owner] += 200 * @rental_days
-  #     elsif option == "additional_insurance"
-  #       options_parts[:drivy] += 1000 * @rental_days
-  #     end
-  #   end
-  #   return options_parts
-  # end
 
   def call
     options_parts = { owner: 0, drivy: 0 }
